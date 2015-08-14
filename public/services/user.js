e3d319e88e4d8b149decf9834438f3e7116aa935
harshana <harshana4u@gmail.com>
2015-08-14 19:31:10 +0530

    changed to http

--- public/services/user.js
+++ public/services/user.js
@@ -3,12 +3,20 @@ angular.module('MyApp')
 	this.loggedUser = {};
 	
 	this.currentUser = function(obj){
-		var user = $resource('/users/:_id',{id:obj._id},{update: {method:'POST', params:obj }});
-		user.update(obj).$promise.then(function(){
-			alertService.alert('Done! ',' Your profile has been updated! ','success');
-		}, function(){
-			alertService.alert('Oops! ',' Looks like that email is already in use! ','danger');
-		});
+		//var user = $resource('/users/:_id',{id:obj._id},{update: {method:'POST', params:obj }});
+				var deferred = $q.defer();
+				
+				$http.post('/users/'+obj._id, obj)
+					.then(function(data,status,headers,config){
+						alertService.alert('Done! ',' Your profile has been updated! ','success');
+						deferred.resolve(data);
+					},function(data,status,headers,config){
+						alertService.alert('Oops! ',' Looks like that email is already in use! ','danger');
+						deferred.reject(status);
+					});
+				
+				return deferred.promise;
+		
 	};
 	
 });
