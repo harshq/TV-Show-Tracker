3fa14b74434f8443727634b480dfeea157e936c0
harshana <harshana4u@gmail.com>
2015-09-25 09:49:55 +0530

    console error fixed in request view

--- public/filters/imageThumb.js
+++ public/filters/imageThumb.js
@@ -1,6 +1,17 @@
 angular.module('MyApp')
 .filter('thumbnail', function(){
 	return function(val){
+
+
+	if(val){
 		return val.replace('original','thumb');
+	}else{
+		return val;
+	}
+
+		
+	
+
+
 	}
 });
